6019056f118e58d4957e6eb259c5edf2fe85682b
MBB Quality Gate Portal <quality_gate_portal.mbb@nokia.com>
2018-09-08 01:52:14 +0800

    2018/09/07 update

--- excel_merge.py
+++ excel_merge.py
@@ -1,3 +1,5 @@
+#coding=utf-8
+
 # This functionality is to merge the excel file under input_data folder to one single file output_data.
 import sys
 import os
@@ -9,23 +11,33 @@ OUTPUT_FOLDER = "./output_data/"
 
 output_length = 0
 
-def is_sheet_header():
-	return False
+def is_sheet_header(row_value):
+	if row_value[0] == u"项目1":
+		print "this is header"
+	return True if u"项目1" in row_value else False 
 
 def handle_work_book(sheet_data, output):
+	global output_length
 	for i in range(sheet_data.nrows):
+		row_data = sheet_data.row_values(i)
 		# the row 0 is sheet name
-		if i == 0:
+		if is_sheet_header(row_data):
 			continue
-		global output_length
-		output_line = output_length
+
+		output_length = output_length + 1
 		for j in range(sheet_data.ncols):
-			print "write data on row :" + str(output_line)
-			output.write(output_line, j, sheet_data.cell(i, j).value)
-		output_length = output_line + 1	
+			#print "write data on row :" + str(output_length)
+			output.write(output_length, j, sheet_data.cell(i, j).value)
 
-
+def write_header(input, output):
+	file_with_path = INPUT_FOLDER + input[0]
+	sheet_data = xlrd.open_workbook(file_with_path).sheet_by_index(0)
+	for i in range(sheet_data.ncols):
+		output.write(0, i, sheet_data.cell(0, i).value)
+			
 def merge_file(input, output):
+
+	write_header(input, output)
 	for file in input:
 		file_with_path = INPUT_FOLDER + file
 		sheet_data = xlrd.open_workbook(file_with_path).sheet_by_index(0)
@@ -51,7 +63,9 @@ def open_output_file():
 
 def main():
 	output_file = open_output_file()
+
 	output_sheet = output_file.add_sheet('result', cell_overwrite_ok=True)
+	
 	input_file_list = read_input_file()
 
 	if output_file is not None and len(input_file_list) > 0:
